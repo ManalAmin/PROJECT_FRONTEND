580c215d2fa872acd89c895e45091a0339475baf
Manal Amin <manalamin657@gmail.com>
2025-03-23 21:30:26 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import { FaTrashAlt } from "react-icons/fa";
 import "./App.css";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "http://localhost:5000/api/projects";
+
 
 function App() {
   const [projects, setProjects] = useState([]);
